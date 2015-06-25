aa956f983ba29d2a832fa66d6bf88e8f627c674b
Alan Chandler <alan.chandler@intrepidlearning.com>
2015-06-25 15:27:04 -0700

    Setting up my drag and drop knight

---
+++ src/Knight.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Knight {
+  render() {
+    return (
+      <span style={{
+        fontSize: '90px'
+        }}>
+        ♘
+      </span>
+    );
+  }
+}
